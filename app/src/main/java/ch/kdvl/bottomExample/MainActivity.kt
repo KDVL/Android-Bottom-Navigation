5fe40e4357b2b8d9880fc40883678ec970ccfef5
KDVL <kevin.dovale@heig-vd.ch>
2020-12-11 18:45:23 +0100

    improve example

--- app/src/main/java/ch/kdvl/bottomExample/MainActivity.kt
+++ app/src/main/java/ch/kdvl/bottomExample/MainActivity.kt
@@ -1,22 +1,53 @@
 package ch.kdvl.bottomExample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import ch.kdvl.bottomNavigation.SpaceNavigationView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import ch.kdvl.bottomNavigation.SpaceItem
+import ch.kdvl.bottomNavigation.SpaceNavigationView
+import ch.kdvl.bottomNavigation.SpaceOnClickListener
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var spaceNavigationView: SpaceNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val spaceNavigationView = findViewById<View>(R.id.nav_view) as SpaceNavigationView
+        spaceNavigationView = findViewById<View>(R.id.nav_view) as SpaceNavigationView
         spaceNavigationView.initWithSaveInstanceState(savedInstanceState)
-        spaceNavigationView.addSpaceItem(SpaceItem("HOME", R.drawable.ic_home_black_24dp))
-        spaceNavigationView.addSpaceItem(SpaceItem("SEARCH", R.drawable.ic_notifications_black_24dp))
+        spaceNavigationView.addSpaceItem(SpaceItem("", R.drawable.ic_tab_events))
+        spaceNavigationView.addSpaceItem(SpaceItem("", R.drawable.ic_tab_search))
+        spaceNavigationView.addSpaceItem(SpaceItem("", R.drawable.ic_tab_favorites))
+        spaceNavigationView.addSpaceItem(SpaceItem("", R.drawable.ic_tab_more))
         spaceNavigationView.setCentreButtonSelectable(true)
         spaceNavigationView.showIconOnly()
+        spaceNavigationView.setInitalSelectedItem(-1)
+
+        spaceNavigationView.setSpaceOnClickListener(object : SpaceOnClickListener {
+            override fun onCentreButtonClick() {
+                Toast.makeText(this@MainActivity, "onCentreButtonClick", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onItemClick(itemIndex: Int, itemName: String) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "$itemIndex $itemName",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onItemReselected(itemIndex: Int, itemName: String) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "$itemIndex $itemName",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
     }
 }
